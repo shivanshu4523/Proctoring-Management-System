1affd8041bdeffb8ade01bed161f30204b38d7cc
Shivanshu Srivastava <shivanshusrivastava440@gmail.com>
2024-08-12 20:14:34 +0530

    Resolved merge conflicts

--- Backend/controller.js
+++ Backend/controller.js
@@ -156,7 +156,7 @@ const signUp = async (req, res) => {
         data.password = await bcrypt.hash(data.password, 5);
         await coll.insertOne(data);
 
-        // const token = jwt.sign({ email: data.email }, JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ email: data.email }, JWT_SECRET, { expiresIn: '1h' });
 
 
         client.close();
