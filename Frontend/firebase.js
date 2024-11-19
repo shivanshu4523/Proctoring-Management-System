03f5912effdbc365b7ba6762e1ca684880d2b3fb
Shivanshu Srivastava <shivanshusrivastava440@gmail.com>
2024-11-19 11:29:20 +0530

    final commit

--- Frontend/firebase.js
+++ Frontend/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage"; // If you're using Firebase Stora
 // Your Firebase config object
 const firebaseConfig = {
     apiKey: "AIzaSyBRqlcQvbM5QfzVNUsOaIVsVMZhg9UghVw",
-        authDomain: "disipline-360.firebaseapp.com",
-        projectId: "disipline-360",
-        storageBucket: "disipline-360.appspot.com",
-        messagingSenderId: "1083655016878",
-        appId: "1:1083655016878:web:1759d2bcd4ca794081783f",
-        measurementId: "G-3LSRBM3XWR"
+    authDomain: "disipline-360.firebaseapp.com",
+    projectId: "disipline-360",
+    storageBucket: "disipline-360.appspot.com",
+    messagingSenderId: "1083655016878",
+    appId: "1:1083655016878:web:1759d2bcd4ca794081783f",
+    measurementId: "G-3LSRBM3XWR"
 };
 
 // Initialize Firebase
